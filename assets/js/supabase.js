5b153f654200ff253a517b2c00be1a4f1be34603
Caleb12307L <caleb12307lk@gmail.com>
2026-08-17 07:01:18 -0600

    Delete assets directory

--- assets/js/supabase.js
+++
@@ -1,8 +0,0 @@
-const SUPABASE_URL = "https://vhheueueddkerfwpdabr.supabase.co";
-
-const SUPABASE_KEY = "sb_publishable_kbNMPf-ZCx5Rwyd_OWNFbg_HRipRw3w";
-
-const supabaseClient = window.supabase.createClient(
-SUPABASE_URL,
-SUPABASE_KEY
-);
